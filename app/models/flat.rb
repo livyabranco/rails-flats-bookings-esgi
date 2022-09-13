42a86711d01109db7e2dfdcff231e984afec14fe
Livya Branco <livyabranco@gmail.com>
2022-09-13 20:17:13 +0200

    validation flat is done

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+  validates :name, :address, :description, :price_per_night, :number_of_guests, presence: true
+  validates :price_per_night, numerically: { greater_than: 0 }
 end
